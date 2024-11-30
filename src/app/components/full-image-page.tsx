a0ca33d8811494a41f1cb59439cadda26cdb9227
djblanc360 <djblanc360@gmail.com>
2024-11-29 18:33:37 -0800

    finalize modal and parallel image routes

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,16 +1,27 @@
+import { clerkClient } from '@clerk/nextjs/server';
 import { getImage } from "~/server/queries";
 
-export default async function FullPageImageViewl(props:{id: number}) {
+export default async function FullPageImageView(props:{id: number}) {
     const image = await getImage(props.id);
+    const client = await clerkClient();
+    const uploaderInfo = await client.users.getUser(image.userId);
+
     return (
         <div className="flex w-full h-full min-w-0">
-            <div className="flex-shrink felx justify-center items-center">
-                <img src={image.url} alt={image.name} className="w-96 object-contain" />
+            <div className="flex-shrink flex justify-center items-center w-2/3">
+                <img src={image.url} alt={image.name} className="w-full object-contain" />
             </div>
             
-
             <div className="w-48 flex flex-col flex-shrink-0 border-l">
-                <h1 className="text-xl font-bold">Lorem ipsum dolor</h1>
+                <h1 className="text-lg border-b text-center p-2">Lorem ipsum dolor</h1>
+                <div className="flex flex-col p-2">
+                    <span>Uploaded By:</span>
+                    <span>{uploaderInfo.fullName}</span>
+                </div>
+                <div className="flex flex-col p-2">
+                    <span>Created on:</span>
+                    <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+                </div>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, et quis, sequi laboriosam id sed tenetur modi, nihil error eius esse corporis atque assumenda porro. Eveniet asperiores ducimus tempora quia!
                 </p>
